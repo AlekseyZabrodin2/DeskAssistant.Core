2ba0bfa9ca652abaa7dbcc70f4656b97ec63ca33
ADANI\zabrodin <rediskal50@gmail.com>
2025-11-25 17:01:25 +0300

    ref: refactoring ITelegramNotificationService

--- DeskAssistant.Core/Services/ITelegramNotificationService.cs
+++ DeskAssistant.Core/Services/ITelegramNotificationService.cs
@@ -4,8 +4,11 @@ namespace DeskAssistant.Core.Services
 {
     public interface ITelegramNotificationService
     {
+        Task<List<CalendarTaskEntity>> GetAllTasksAsync();
+        Task<List<CalendarTaskEntity>> GetTasksForTodayAsync();
+        Task<CalendarTaskEntity> AddTaskAsync(CalendarTaskEntity taskEntity);
+        Task UpdateTaskAsync(CalendarTaskModel taskEntity, TaskStatusEnum status);
         Task NotifyTaskCreatedAsync(CalendarTaskModel notification);
         Task NotifyTaskCompletedAsync(CalendarTaskModel notification);
-
     }
 }
