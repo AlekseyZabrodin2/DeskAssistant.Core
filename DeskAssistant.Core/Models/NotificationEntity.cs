7f76ccf0b6e488d33681561c3eb586e303c46d1d
ADANI\zabrodin <rediskal50@gmail.com>
2025-12-03 15:04:47 +0300

    feat: create Notification proto

---
+++ DeskAssistant.Core/Models/NotificationEntity.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DeskAssistant.Core.Models
+{
+    public partial class NotificationEntity : ObservableObject
+    {
+        [ObservableProperty]
+        public partial string Id { get; set; }
+
+        [ObservableProperty]
+        public partial string ClientId { get; set; }
+        [ObservableProperty]
+        public partial bool IsEnabled { get; set; }
+        [ObservableProperty]
+        public partial TimeSpan NotificationTime { get; set; }
+
+        [ObservableProperty]
+        public partial bool MondayEnabled { get; set; }
+        [ObservableProperty]
+        public partial bool TuesdayEnabled { get; set; }
+        [ObservableProperty]
+        public partial bool WednesdayEnabled { get; set; }
+        [ObservableProperty]
+        public partial bool ThursdayEnabled { get; set; }
+        [ObservableProperty]
+        public partial bool FridayEnabled { get; set; }
+        [ObservableProperty]
+        public partial bool SaturdayEnabled { get; set; }
+        [ObservableProperty]
+        public partial bool SundayEnabled { get; set; }
+
+        [ObservableProperty]
+        public partial DateTime CreatedAt { get; set; }
+    }
+}
---
+++ DeskAssistant.Core/Services/INotificationService.cs
@@ -0,0 +1,17 @@
+﻿using DeskAssistant.Core.Models;
+
+namespace DeskAssistant.Core.Services
+{
+    public interface INotificationService
+    {
+        Task<List<NotificationEntity>> GetNotificationsSettingsAsync(string clientId);
+        Task GetNotificationStatus(NotificationEntity notification);
+        Task CreatNotificationsAsync(NotificationEntity notification); 
+        Task DeleteNotificationAsync(NotificationEntity notification);
+        Task ActivateNotificationsAsync(NotificationEntity notification);
+        Task DisableNotificationsAsync(NotificationEntity notification);
+
+        Task StartNotificationServiceAsync();
+        Task StopNotificationServiceAsync();
+    }
+}
